59dead7d072b7a573c4c21873454368bd84eee8c
MrWither83 <52672543+MrWither83@users.noreply.github.com>
2019-10-21 23:37:55 +0200

    Stuff

--- js/game.js
+++ js/game.js
@@ -1,3 +1,22 @@
 player = {
     owo: new Decimal(0),
 }
+
+function clickowo() {
+    player.owo = player.owo.add(1);
+}
+
+function update() {
+
+}
+
+function display() {
+    document.getElementById("owo").innerHTML = player.owo.toString()
+}
+
+function gameLoop() {
+    display();
+    update();
+}
+
+setInterval(gameLoop, 33)
